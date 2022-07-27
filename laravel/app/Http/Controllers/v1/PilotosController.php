5262c9acd835404f70a1d7e459fac0a4ce8b299f
devureta <devureta@gmail.com>
2022-07-27 18:57:16 -0500

    add GET/POST/PUT/PATCH/DELETE

--- laravel/app/Http/Controllers/v1/PilotosController.php
+++ laravel/app/Http/Controllers/v1/PilotosController.php
@@ -2,17 +2,137 @@
 
 namespace App\Http\Controllers\v1;
 
-use App\Models\Piloto;
 use App\Http\Controllers\Controller;
+use App\Models\Piloto;
+use Illuminate\Http\Request;
 use App\Models\User;
 
 class PilotosController extends Controller
 {
-    public function getPilotos()
+    /* GET */
+    public function getAll()
     {
         $response_local=new \stdClass();
+        $pilotos = Piloto::all();
         $response_local->success=true;
-        $response_local->data=Piloto::all();
+        $response_local->data=$pilotos;
+        return response()->json($response_local,200);
+    }
+
+    /* GET->ITEM */
+    public function getItem($id)
+    {
+        $response_local=new \stdClass();
+
+        $piloto = Piloto::find($id);
+
+        $response_local->data=$piloto;
+        $response_local->success=true;
+        return response()->json($response_local,200);
+    }
+
+    /* POST */
+    public function store(Request $request)
+    {
+        $response_local=new \stdClass();
+
+        $piloto = new Piloto();
+        $piloto->id=$request->id;
+        $piloto->nombres=$request->nombres;
+        $piloto->ap_Paterno=$request->ap_Paterno;
+        $piloto->ap_Materno=$request->ap_Materno;
+        $piloto->fechaNacimiento=$request->fechaNacimiento;
+        $piloto->edad=$request->edad;
+        $piloto->pais=$request->pais;
+        $piloto->ciudad=$request->ciudad;
+        $piloto->save();
+
+        $response_local->data=$piloto;
+        $response_local->success=true;
+        return response()->json($response_local,200);
+    }
+
+    /* PUT */
+    public function update(Request $request,$id)
+    {
+        $response_local=new \stdClass();
+
+        $piloto = Piloto::find($id);
+        $piloto->id=$request->id;
+        $piloto->nombres=$request->nombres;
+        $piloto->ap_Paterno=$request->ap_Paterno;
+        $piloto->ap_Materno=$request->ap_Materno;
+        $piloto->fechaNacimiento=$request->fechaNacimiento;
+        $piloto->edad=$request->edad;
+        $piloto->pais=$request->pais;
+        $piloto->ciudad=$request->ciudad;
+        $piloto->save();
+
+        $response_local->success=true;
+        return response()->json($response_local,200);
+    }
+
+    /* PATCH */
+    public function patchUpdate(Request $request,$id)
+    {
+        $response_local=new \stdClass();
+        $piloto = Piloto::find($id);
+
+        if($request->id!=null)
+        {
+            $piloto->id=$request->id;
+        }
+
+        if($request->nombres!=null)
+        {
+            $piloto->nombres=$request->nombres;
+        }
+
+        if($request->ap_Paterno!=null)
+        {
+            $piloto->ap_Paterno=$request->ap_Paterno;
+        }
+
+        if($request->ap_Materno!=null)
+        {
+            $piloto->ap_Materno=$request->ap_Materno;
+        }
+
+        if($request->fechaNacimiento!=null)
+        {
+            $piloto->fechaNacimiento=$request->fechaNacimiento;
+        }
+
+        if($request->edad!=null)
+        {
+            $piloto->edad=$request->edad;
+        }
+
+        if($request->pais!=null)
+        {
+            $piloto->pais=$request->pais;
+        }
+
+        if($request->ciudad!=null){
+            $piloto->ciudad=$request->ciudad;
+        }
+
+        $piloto->save();
+
+        $response_local->data=$piloto;
+        $response_local->success=true;
+        return response()->json($response_local,200);
+    }
+
+    /* DELETE */
+    public function delete($id)
+    {
+        $response_local=new \stdClass();
+
+        $piloto = Piloto::find($id);
+        $piloto->delete();
+
+        $response_local->success=true;
         return response()->json($response_local,200);
     }
 }
--- laravel/routes/api.php
+++ laravel/routes/api.php
@@ -19,4 +19,15 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
 
-Route::get('/v1/pilotos', [PilotosController::class, 'getPilotos']);
+/* GET */
+Route::get('/v1/pilotos', [PilotosController::class, 'getAll']);
+/* GET->ITEM */
+Route::get('/v1/pilotos/{id}', [PilotosController::class, 'getItem']);
+/* POST */
+Route::post('/v1/pilotos', [PilotosController::class, 'store']);
+/* PUT */
+Route::put('/v1/pilotos/{id}', [PilotosController::class, 'update']);
+/* PATCH */
+Route::patch('/v1/pilotos/{id}', [PilotosController::class, 'patchUpdate']);
+/* DELETE */
+Route::delete('/v1/pilotos/{id}', [PilotosController::class, 'delete']);
